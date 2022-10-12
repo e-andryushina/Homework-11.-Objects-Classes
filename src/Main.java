6c1730396f0f9e183992b10a6993d85d5886938b
e-andryushina <elena-and@inbox.ru>
2022-10-12 22:39:36 +0300

    ДЗ к Уроку Объекты и методы

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Author author = new Author("Джером", "Сэлинджер");
+        Book book = new Book("Над пропастью во ржи",  author, 2005);
+
+        Author author2 = new Author ("Антон", "Чехов");
+        Book book2 = new Book ("Вишневый сад", author2 , 1965 );
+        System.out.println("Название первой книги: " + book.getBookName() +
+                " , автор: " + author.getAuthorName() + " " +
+                author.getAuthorSurname() +
+                " , год издания: " + book.getYearOfPublication());
+        System.out.println("Название второй книги: " + book2.getBookName() +
+                " , автор: " + author2.getAuthorName() + " " + author2.getAuthorSurname() +
+                " , год издания: " + book2.getYearOfPublication() );
+
+        book.setYearOfPublication(2010);
+        System.out.println("Книга " + book.getBookName() + " была переиздана в " + book.getYearOfPublication() + " году");
+
     }
 }
