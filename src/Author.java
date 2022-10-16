fc3de2f10b363f156256e66293b7b8940b56ca7f
e-andryushina <elena-and@inbox.ru>
2022-10-16 05:46:14 +0300

    ДЗ к Уроку Методы объектов

--- src/Author.java
+++ src/Author.java
@@ -1,18 +1,37 @@
+import java.util.Objects;
+
 public class Author {
     private final String authorName;
     private final String authorSurname;
 
-    public Author (String authorName, String authorSurname) {
+    public Author(String authorName, String authorSurname) {
         this.authorName = authorName;
         this.authorSurname = authorSurname;
     }
 
-    public String getAuthorName () {
+    public String getAuthorName() {
         return this.authorName;
     }
-    public String getAuthorSurname () {
+
+    public String getAuthorSurname() {
         return this.authorSurname;
     }
 
+    public String toString() {
+        return this.authorName + " " + this.authorSurname;
 
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return authorName.equals(author.authorName) && authorSurname.equals(author.authorSurname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(authorName, authorSurname);
+    }
 }
